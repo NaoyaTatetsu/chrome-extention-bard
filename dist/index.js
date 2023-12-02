42bc87c3cb034a807bea5c14bf21d453a8b2be37
NaoyaTatetsu <tatetsu_naoya@cyberagent.co.jp>
2023-12-03 06:15:36 +0900

    update: build index.ts with esbuild

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,1 @@
-"use strict";
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({ url: 'https://bard.google.com/chat' });
-});
+"use strict";(()=>{chrome.action.onClicked.addListener(()=>{chrome.tabs.create({url:"https://bard.google.com/chat"})});})();
